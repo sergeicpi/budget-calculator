08be8575c0584ca280134e8b7ec0effdc479d9ff
Master <sergeicpi@yandex.ru>
2022-06-03 16:21:53 +0300

    Create repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# budget-app
+
+## HTML5, CSS3, JS, Vue 2.6
+
+- List of income and expenses
+- Add/Remove & calculate
+
+![budget-app.jpg](budget-app.jpg)
---
+++ src/components/Form.vue
@@ -0,0 +1,79 @@
+<template>
+  <ElCard class="form-card">
+    <ElForm
+      :model="formData"
+      ref="addItemForm"
+      :rules="rules"
+      lable-position="top"
+    >
+      <ElFormItem label="Type" prop="type">
+        <ElSelect
+          class="type-select"
+          v-model="formData.type"
+          placeholder="Choose type..."
+        >
+          <ElOption lable="Income" value="INCOME" />
+          <ElOption lable="Outcome" value="OUTCOME" />
+        </ElSelect>
+      </ElFormItem>
+      <ElFormItem label="Comments" prop="comment">
+        <ElInput v-model="formData.comment" />
+      </ElFormItem>
+      <ElFormItem label="Value" prop="value">
+        <ElInput v-model.number="formData.value" />
+      </ElFormItem>
+      <ElButton @click="onSubmit" type="primary">Submit</ElButton>
+    </ElForm>
+  </ElCard>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data: () => ({
+    formData: {
+      type: 'INCOME',
+      comment: '',
+      value: 0,
+    },
+    rules: {
+      type: [
+        { required: true, message: 'Please select type', trigger: 'blur' },
+      ],
+      comment: [
+        { required: true, message: 'Please input comment', trigger: 'blur' },
+      ],
+      value: [
+        { required: true, message: 'Please select value', trigger: 'change' },
+        {
+          type: 'number',
+          message: 'Value must be a number',
+          trigger: 'change',
+        },
+      ],
+    },
+  }),
+  methods: {
+    onSubmit() {
+      // console.log(this.$refs.addItemForm)
+      this.$refs.addItemForm.validate((valid)=>{
+        // console.log(valid)
+        if (valid) {
+          this.$emit('submitForm', {...this.formData})
+          this.$refs.addItemForm.resetFields()
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.form-card {
+  max-width: 500px;
+  margin: auto;
+}
+.type-select {
+  width: 100%;
+}
+</style>
